4114a2ba7df9033e534e855c10246dc7bb9e823b
Võ Hoàng Phúc <contact@vohoangphuc.com>
2026-06-13 18:57:08 +0700

    feat(starter): install tuturuuu ui and sdk

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["@tuturuuu/ui"],
+  transpilePackages: [
+    "@tuturuuu/ai",
+    "@tuturuuu/apis",
+    "@tuturuuu/google",
+    "@tuturuuu/hooks",
+    "@tuturuuu/icons",
+    "@tuturuuu/internal-api",
+    "@tuturuuu/supabase",
+    "@tuturuuu/types",
+    "@tuturuuu/ui",
+    "@tuturuuu/utils",
+  ],
 };
 
 export default nextConfig;
